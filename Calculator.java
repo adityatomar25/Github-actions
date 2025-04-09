fe6e1616494ec6aec5d91974043a7ff1734191bc
Aditya Tomar <143313634+adityatomar25@users.noreply.github.com>
2025-04-09 14:45:38 +0530

    Create Calculator.java

---
+++ Calculator.java
@@ -0,0 +1,26 @@
+public class Calculator {
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public int divide(int a, int b) {
+        if (b == 0) throw new IllegalArgumentException("Cannot divide by zero");
+        return a / b;
+    }
+
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+        System.out.println("Addition: " + calc.add(5, 3));
+        System.out.println("Subtraction: " + calc.subtract(5, 3));
+        System.out.println("Multiplication: " + calc.multiply(5, 3));
+        System.out.println("Division: " + calc.divide(6, 3));
+    }
+}
